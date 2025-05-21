51101008ef0b0eb9c033a5d5e9dd20a62d8d38f1
JULDD LLC <julie@juldd.com>
2025-05-21 18:37:20 -0500

    Fix next command path in build script

--- build-cf.js
+++ build-cf.js
@@ -3,8 +3,9 @@ const { execSync } = require('child_process');
 
 console.log('🚀 Running custom Cloudflare Pages build script');
 console.log('1. Running next build');
-execSync('next build', { stdio: 'inherit' });
+execSync('npx next build', { stdio: 'inherit' }); // Changed to use npx
 console.log('2. Building edge functions');
 execSync('npx @cloudflare/next-on-pages', { stdio: 'inherit' });
 console.log('✅ Build complete!');
+
 
